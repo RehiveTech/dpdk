0ee11165b36b8533a26b194263d58bf5551bc63d
Jan Viktorin <viktorin@rehivetech.com>
2016-07-08 19:49:42 +0200

    eal: no need to test for PMD_VDEV anymore
    All devices in the rte_eal_vdev_init/uninit are always virtual devices.

--- lib/librte_eal/common/eal_common_vdev.c
+++ lib/librte_eal/common/eal_common_vdev.c
@@ -60,9 +60,6 @@ rte_eal_vdev_init(const char *name, const char *args)
 		return -EINVAL;
 
 	TAILQ_FOREACH(driver, &vdev_driver_list, next) {
-		if (driver->driver.type != PMD_VDEV)
-			continue;
-
 		/*
 		 * search a driver prefix in virtual device name.
 		 * For example, if the driver is pcap PMD, driver->name
@@ -86,9 +83,6 @@ rte_eal_vdev_uninit(const char *name)
 		return -EINVAL;
 
 	TAILQ_FOREACH(driver, &vdev_driver_list, next) {
-		if (driver->driver.type != PMD_VDEV)
-			continue;
-
 		/*
 		 * search a driver prefix in virtual device name.
 		 * For example, if the driver is pcap PMD, driver->name
